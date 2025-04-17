253952c871ad14bf656bbc97362083aaa4453f60
Ian Wafula <neocorpse@proton.me>
2025-04-17 06:04:01 +0300

    Dragon moto testing moto testing, waaaah 🐉🔥

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
 // Your code here
+export function pointsForWord(word) {
+    const letters = word.split('')
+    return letters.reduce((total, letter) => {
+       return total += /[aeiou]/i.test(letter) ? 1 : 2;
+    }, 0)
+}
